baeb4b4330c0f700cc7a719536e2d1e7005fbfe8
Danikya <daniquebodt@hotmail.com>
2017-03-31 21:24:45 +0200

    Definitieve versie
    ORF finder Project groep 10

--- FileHandler/NotNuclSequence.java
+++ FileHandler/NotNuclSequence.java
@@ -2,6 +2,10 @@
 package FileHandler;
 
 public class NotNuclSequence extends Exception{
+    /**
+     * De custom exception wordt opgeroepen wanneer er geen DNA sequentie als input wordt gegeven
+     * 
+     */
     public NotNuclSequence() {
         super();
     }
--- sql_con/SQL_con.java
+++ sql_con/SQL_con.java
@@ -9,7 +9,6 @@ import FileHandler.FileReader;
 import java.sql.*;
 import java.util.ArrayList;
 import ORF.findORF;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,83 +18,78 @@ public class SQL_con {
 
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://xirect.nl:3306/";
-    static final String dbName = "pg10";
-
+    static final String DB_NAME = "pg10";
     static final String USER = "pg10";
     static final String PASS = "blaat1234";
-
+    
+    static ResultSet row_in_database;
+    static ArrayList<Object> all_results_database = new ArrayList<>();
+    
+    /**
+     * Deze methode connect met de database en vult de database met de gevonden ORF statistieken van de desbetreffende sequentie.
+     */
     public void connect() {
+        ArrayList<Object> countListDNA = new ArrayList<Object>();
         Connection conn = null;
         Statement stmt = null;
         int dnaID = 0;
 
         try {
-            
             findORF orf = new findORF();
-      
             FileReader reader = new FileReader();
-            String tempStr = reader.getTemplateStrand();
-            orf.setORF(settings);
-            System.out.println(orf.getORFstartComp().size());
+            
+            // laden van de JDBC Driver
             Class.forName("com.mysql.jdbc.Driver");
-
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL + dbName, USER, PASS);
-            System.out.println("Connected database successfully...");
-
-            ArrayList<Object> dnacountlist = new ArrayList<Object>();
+            
+            // Connectie met de database.
+            conn = DriverManager.getConnection(DB_URL + DB_NAME, USER, PASS);
+           
             stmt = conn.createStatement();
+            
+            // HIER MOET JE EVENTJES BIJZETTEN WAT HET DOET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             ResultSet result = stmt.executeQuery("SELECT ORFfinder_ID FROM DNA");
+            
             while (result.next()) {
-
-                dnacountlist.add(new Integer(result.getInt(1)));
-
+                countListDNA.add(new Integer(result.getInt(1)));
             }
-            dnaID = dnacountlist.size() + 1;
-
-            System.out.println("Inserting records into the table...");
+            
+            dnaID = countListDNA.size() + 1;
             stmt = conn.createStatement();
-
+            
+            // Toevoegen van het dnaID en de template strand.
             String sql = "INSERT INTO DNA(ORFfinder_ID, DNA_seq)"
-                    + "VALUES (" + dnaID + ",'" + tempStr + "')";
+                    + "VALUES (" + dnaID + ",'" + reader.getTemplateStrand() + "')";
             stmt.executeUpdate(sql);
-            System.out.println("Inserted records into the table...");
-
-            System.out.println("Start ORF querrys");
             stmt = null;
             stmt = conn.createStatement();
-
+            
+            // Toevoegen van de gegevens van het ORF aan het dnaID van de desbetreffende sequentie.
             int ORFcount = 0;
             while (ORFcount < orf.getORFsequenceComp().size()) {
-
-                sql = "INSERT INTO ORF(DNA_ID_fkey, ORFseq,ReadingFrame,start,stop)"
-                        + "VALUES (" + dnaID + ",'" + orf.getORFsequenceComp().get(ORFcount) + "','-" + orf.getORFrfComp().get(ORFcount) + "'," + orf.getORFstartComp().get(ORFcount) + "," + orf.getORFendComp().get(ORFcount) + ")";
+                sql = "INSERT INTO ORF(DNA_ID_fkey, ORFseq,ReadingFrame,start,stop)" + "VALUES (" + dnaID + ",'" + orf.getORFsequenceComp().get(ORFcount) + "','-" + orf.getORFrfComp().get(ORFcount) + "'," + orf.getORFstartComp().get(ORFcount) + "," + orf.getORFendComp().get(ORFcount) + ")";
                 stmt.executeUpdate(sql);
                 ORFcount++;
-                System.out.println("Inserted ORF records into the table...");
             }
-            System.out.println("Start ORFTemp querrys");
+            
             stmt = null;
             stmt = conn.createStatement();
 
-            int ORFcountTemp = 0;
-            while (ORFcountTemp < orf.getORFsequenceTemp().size()) {
-
+            int countTempORF = 0;
+            
+            // Toevoegen van de gegevens van het ORF aan het dnaID van de desbetreffende sequentie.
+            while (countTempORF < orf.getORFsequenceTemp().size()) {
                 sql = "INSERT INTO ORF(DNA_ID_fkey, ORFseq,ReadingFrame,start,stop)"
-                        + "VALUES (" + dnaID + ",'" + orf.getORFsequenceTemp().get(ORFcountTemp) + "','+" + orf.getORFrfTemp().get(ORFcountTemp) + "'," + orf.getORFstartTemp().get(ORFcountTemp) + "," + orf.getORFendTemp().get(ORFcountTemp) + ")";
+                        + "VALUES (" + dnaID + ",'" + orf.getORFsequenceTemp().get(countTempORF) + "','+" + orf.getORFrfTemp().get(countTempORF) + "'," + orf.getORFstartTemp().get(countTempORF) + "," + orf.getORFendTemp().get(countTempORF) + ")";
                 stmt.executeUpdate(sql);
-                ORFcountTemp++;
-                System.out.println("Inserted ORF records into the table...");
+                countTempORF++;
+
             }
 
         } catch (SQLException se) {
-
             se.printStackTrace();
         } catch (Exception e) {
-
             e.printStackTrace();
         } finally {
-
             try {
                 if (stmt != null) {
                     conn.close();
@@ -110,6 +104,51 @@ public class SQL_con {
                 se.printStackTrace();
             }
         }
-        System.out.println("Goodbye!");
+    }
+    
+    /**
+     * Deze methode haalt alles op uit de database en slaat het op in een arraylist zodat de arraylist opgehaald kan 
+     * worden doormiddel van een getter.
+     */
+    public void setDatabaseItems(){
+        try {
+            Connection conn = null;
+            Statement stmt = null;
+            
+            // De kolomnamen toevoegen aan de arraylist zodat deze doormiddel van een getter opgehaald kunnen worden.
+            all_results_database.add("ORF ID" + "\t" + "Reading frame"
+                        + "\t" + "Start positie"+ "\t" + "Stop positie"
+                        + "\t" +"\n");
+            
+            // Laden van de JDBC Driver. 
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            // Connectie maken met de SQL database. 
+            conn = DriverManager.getConnection(DB_URL + DB_NAME, USER, PASS);
+            String query = "select ORFfinder_ID,ReadingFrame,start,stop from DNA inner join ORF on DNA.ORFfinder_ID = ORF.DNA_ID_fkey";       
+            stmt = conn.createStatement();
+            SQL_con.row_in_database = stmt.executeQuery(query);
+            
+            // Het ophalen van alle gegevens uit de database en in de arraylist zetten zodat de gegevens opgehaald kunnen worden 
+            // doormiddel van een getter. 
+            while (SQL_con.row_in_database.next()) {          
+                all_results_database.add(SQL_con.row_in_database.getInt("ORFfinder_ID") + "\t");
+                all_results_database.add(SQL_con.row_in_database.getString("ReadingFrame") + "\t");
+                all_results_database.add(SQL_con.row_in_database.getInt("start") + "\t");
+                all_results_database.add(SQL_con.row_in_database.getInt("stop") + "\n");
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 
+     * @return all_results_database     Ophalen van alle gegevens in de database.
+     */
+    public ArrayList getDatabaseItems(){
+        return all_results_database;
     }
 }
